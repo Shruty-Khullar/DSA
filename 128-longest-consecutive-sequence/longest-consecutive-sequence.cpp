2691355117af5c06ee6f505a2f57332289223570
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-05 22:18:31 +0530

    Time: 85 ms (40.56%) | Memory: 89.1 MB (16.49%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0)
-            return 0;
-        sort(nums.begin(), nums.end());
-        int max_length=0;
-        int currSeq=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1]==1){
-                currSeq++;
-            } else if(nums[i]-nums[i-1]==0) {
+        // int n = nums.size();
+        // if(n==0)
+        //     return 0;
+        // sort(nums.begin(), nums.end());
+        // int max_length=0;
+        // int currSeq=1;
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]-nums[i-1]==1){
+        //         currSeq++;
+        //     } else if(nums[i]-nums[i-1]==0) {
+        //         continue;
+        //     } else {
+        //         currSeq=1;
+        //     }
+        //     max_length=max(max_length, currSeq);
+        // }
+        // max_length=max(max_length, currSeq);
+        // return max_length;
+        int max_len=0, subseqLen=0;
+        unordered_set<int> s;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            s.insert(nums[i]);
+        }
+        for(auto itr: s){
+            if(s.find(itr-1)!=s.end()){
                 continue;
             } else {
-                max_length=max(max_length, currSeq);
-                currSeq=1;
+                subseqLen++;
+                int num = itr;
+                while(s.find(num+1)!=s.end()){
+                    subseqLen++;
+                    num=num+1;
+                }
             }
+            max_len=max(max_len, subseqLen);
+            subseqLen=0;
         }
-        max_length=max(max_length, currSeq);
-        return max_length;
+        return max_len;
+
     }
 };
