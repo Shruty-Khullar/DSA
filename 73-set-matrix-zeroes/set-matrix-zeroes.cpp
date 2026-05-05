d8905f27f2143b165682315d7a3d1072dc2595f0
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-05 23:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (75.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,30 +1,78 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int> rows;
-        set<int> cols;
-        int row_num = matrix.size();
-        int col_num = matrix[0].size();
+    //     set<int> rows;
+    //     set<int> cols;
+    //     int row_num = matrix.size();
+    //     int col_num = matrix[0].size();
 
-        for(int i=0;i<row_num;i++){
-            for(int j=0;j<col_num;j++){
-                if(matrix[i][j]==0){
-                    rows.insert(i);
-                    cols.insert(j);
-                }
-            }
+    //     for(int i=0;i<row_num;i++){
+    //         for(int j=0;j<col_num;j++){
+    //             if(matrix[i][j]==0){
+    //                 rows.insert(i);
+    //                 cols.insert(j);
+    //             }
+    //         }
+    //     }
+
+    //     for(auto itr: rows){
+    //         for(int j=0;j<col_num;j++){
+    //             matrix[itr][j]=0;
+    //         }
+    //     }
+
+    //     for(auto itr: cols){
+    //         for(int i=0;i<row_num;i++){
+    //             matrix[i][itr]=0;
+    //         }
+    //     }
+    // }
+
+    int row_num = matrix.size();
+    int col_num = matrix[0].size();
+    bool firstRowZero=false;
+    bool firstColZero=false;
+    for(int i=0;i<row_num;i++){
+        if(matrix[i][0]==0){
+            firstColZero=true;
+            break;
         }
+    }
 
-        for(auto itr: rows){
-            for(int j=0;j<col_num;j++){
-                matrix[itr][j]=0;
-            }
+    for(int j=0;j<col_num;j++){
+        if(matrix[0][j]==0){
+            firstRowZero=true;
+            break;
         }
+    }
 
-        for(auto itr: cols){
-            for(int i=0;i<row_num;i++){
-                matrix[i][itr]=0;
+    for(int i=1;i<row_num;i++){
+        for(int j=1;j<col_num;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0] = 0;
+                matrix[0][j]=0;
             }
         }
     }
+
+    for(int i=1;i<row_num;i++){
+        for(int j=1;j<col_num;j++){
+            if(matrix[i][0] == 0 ||  matrix[0][j]==0){
+                matrix[i][j]=0;
+            }
+        }
+    }
+
+    if(firstColZero){
+        for(int i=0;i<row_num;i++){
+            matrix[i][0]=0;
+        }
+    }
+    if(firstRowZero){
+        for(int i=0;i<col_num;i++){
+            matrix[0][i]=0;
+        }
+    }
+    }
+
 };
