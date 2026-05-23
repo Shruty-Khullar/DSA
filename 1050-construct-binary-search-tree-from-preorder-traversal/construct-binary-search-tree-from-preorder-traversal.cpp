02a0b8b7694fe1cd367b40ee9aebe23a729ae4ea
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-23 15:50:52 +0530

    Time: 2 ms (21.22%) | Memory: 16.5 MB (45.37%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* Helper(vector<int>& preorder, int& index, long bound){
+        if(index == preorder.size() || preorder[index]>bound)
+            return nullptr;
+        TreeNode* root = new TreeNode(preorder[index++]);
+        root->left = Helper(preorder, index, root->val);
+        root->right = Helper(preorder, index, bound);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int index = 0;
+        TreeNode* root = Helper(preorder, index, INT_MAX);
+        return root;
+    }
+};
