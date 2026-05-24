3fd9613d6c2acac6d567f964632d7fec8e5303ba
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-24 13:38:17 +0530

    Time: 4 ms (36.23%) | Memory: 14.1 MB (52.29%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void Helper(vector<int>& candidates, int n,int index, int target, vector<vector<int>>& ans, vector<int>& temp, int sum){
-        if(sum == target){
+    void Helper(vector<int>& candidates, int n,int index, int target, vector<vector<int>>& ans, vector<int>& temp){
+        if(target==0){
             ans.push_back(temp);
             return;
         }
-        if(index==n || sum>target)
+        if(index==n || target<0)
             return;
         
-        sum+=candidates[index];
         temp.push_back(candidates[index]);
-        Helper(candidates,n,index,target,ans,temp,sum);
-        sum-=candidates[index];
+        Helper(candidates,n,index,target-candidates[index],ans,temp);
         temp.pop_back();
-        Helper(candidates,n,index+1,target,ans,temp,sum);
+        Helper(candidates,n,index+1,target,ans,temp);
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
         int n = candidates.size();
-        Helper(candidates, n, 0, target, ans, temp, 0);
+        Helper(candidates, n, 0, target, ans, temp);
         return ans;
         
     }
