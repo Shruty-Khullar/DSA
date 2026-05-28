78f6b1f06d72eb62f6e7c0abc16e9e9dc5717a6b
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-28 13:17:45 +0530

    Time: 7 ms (6.20%) | Memory: 16.6 MB (85.35%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int fresh = 0;
+        queue<pair<int, int>> q;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                } else if(grid[i][j]==1) fresh++;
+            }
+        }
+        if(fresh==0) return 0;
+        int time = 0;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+
+                if(row-1>=0 && grid[row-1][col]==1){
+                    grid[row-1][col]=2;
+                    q.push({row-1, col});
+                } 
+                if(col-1>=0 && grid[row][col-1]==1){
+                    grid[row][col-1]=2;
+                    q.push({row, col-1});
+                } 
+                if(row+1<rows && grid[row+1][col]==1){
+                    grid[row+1][col]=2;
+                    q.push({row+1, col});
+                } 
+                if(col+1<cols && grid[row][col+1]==1){
+                    grid[row][col+1]=2;
+                    q.push({row, col+1});
+                } 
+            }
+            time++;
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return time-1;
+
+    }
+};
