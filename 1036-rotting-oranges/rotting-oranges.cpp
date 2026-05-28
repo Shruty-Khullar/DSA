e0b21c36a47c0994af00c81dfbcc027043794e2d
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-28 13:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (69.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -24,29 +24,34 @@ public:
                 if(row-1>=0 && grid[row-1][col]==1){
                     grid[row-1][col]=2;
                     q.push({row-1, col});
+                    fresh--;
                 } 
                 if(col-1>=0 && grid[row][col-1]==1){
                     grid[row][col-1]=2;
                     q.push({row, col-1});
+                    fresh--;
                 } 
                 if(row+1<rows && grid[row+1][col]==1){
                     grid[row+1][col]=2;
                     q.push({row+1, col});
+                    fresh--;
                 } 
                 if(col+1<cols && grid[row][col+1]==1){
                     grid[row][col+1]=2;
                     q.push({row, col+1});
+                    fresh--;
                 } 
             }
             time++;
         }
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
+        // for(int i=0;i<rows;i++){
+        //     for(int j=0;j<cols;j++){
+        //         if(grid[i][j]==1){
+        //             return -1;
+        //         }
+        //     }
+        // }
+        if(fresh!=0) return -1;
         return time-1;
 
     }
