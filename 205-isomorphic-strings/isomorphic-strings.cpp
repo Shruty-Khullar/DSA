2ec05b0c1f3c61d9543cef4f616eb18c8a081bac
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2026-05-29 13:48:20 +0530

    Time: 4 ms (23.34%) | Memory: 9.3 MB (81.26%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
-        map<char, char> to;
         set<char> used;
-        for(int i = 0; i<s.size(); i++) {
-            if (to.find(s[i]) == to.end()) {
-                if (used.find(t[i]) == used.end()) {
-                    to[s[i]] = t[i];
+        map<char, char> mp;
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])==mp.end()){
+                if(used.find(t[i])==used.end()){
+                    mp[s[i]] = t[i];
                     used.insert(t[i]);
                 }
-                else {
+                else 
                     return false;
-                }
             }
-            s[i] = to[s[i]];
-        }
 
-        return s == t;
+            s[i] = mp[s[i]];
+        }
+        return s==t;
     }
 };
